bb9a392f9d0a002bb590b97228680984ff8a1ed1
Maks Daniłaŭ <47792447+MaxDaniLOVE@users.noreply.github.com>
2020-07-28 17:02:19 +0300

    feat: add logic to heroes component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,13 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-
+import { HEROES } from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = { id: '1', name: 'Windstorm' };
+  heroes = HEROES;
+
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    if (this.selectedHero === hero) { // TODO remove if necessary
+      this.selectedHero = null; 
+    } else {
+      this.selectedHero = hero;
+    }
+  }
 
   constructor() { }
 
